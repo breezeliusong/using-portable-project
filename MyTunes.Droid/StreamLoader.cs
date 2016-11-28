22e95a10c0355ec95fc0f586ea2be361ba079558
v-liuson <v-liuson@MSDNUWPSERVER1>
2016-11-28 14:07:16 +0800

    Add project files.

---
+++ MyTunes.Droid/StreamLoader.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MyTunes.Shared;
+using System.IO;
+
+namespace MyTunes
+{
+    public class StreamLoader: IStreamLoader
+    {
+        private readonly Context context;
+        public StreamLoader(Context context)
+        {
+            this.context = context;
+        }
+
+        public Stream GetStreamForFilename(string filename)
+        {
+            return context.Assets.Open(filename);
+        }
+    }
+}
---
+++ MyTunes.UWP/StreamLoader.cs
@@ -0,0 +1,21 @@
+﻿using MyTunes.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MyTunes.UWP
+{
+    public class StreamLoader : IStreamLoader
+    {
+
+        public  Stream GetStreamForFilename(string filename)
+        {
+            return Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(filename)
+                .AsTask().Result
+                .OpenStreamForReadAsync().Result;
+        }
+    }
+}
